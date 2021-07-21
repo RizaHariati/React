632a77f17da14ef7280d66aaabcbde57e5808518
RizaHariati <rizahariati@yahoo.com>
2021-07-21 13:47:36 +0700

    proptype Tutorial from Coding Addict

--- tutorial_advanced/src/exercise/10-prop-types/Product.js
+++ tutorial_advanced/src/exercise/10-prop-types/Product.js
@@ -1,31 +1,33 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import defaultImage from "../../assets/thanksgiving-3811492_1920.jpg";
 
 const Product = ({ image, name, price }) => {
-  const url = image && image.url;
+  const img = image && image.url;
   return (
     <div className="item">
       <div className="image">
-        <img src={url || defaultImage} alt={name || "default"} />
+        <img src={img || defaultImage} alt={name} />
       </div>
+
       <div className="info">
-        <h4>{name}</h4>
-        <p className={price}>$ {price || 3.99}</p>
+        <h4 className="title">{name}</h4>
+        <p className="price">{price}</p>
       </div>
     </div>
   );
 };
-// Product.propTypes = {
-//   image:PropTypes.object.isRequired,,
-//   name: PropTypes.string.isRequired,
-//   price: PropTypes.number.isRequired,
-// };
 
-// Product.defaultProps = {
-//   image: defaultImage,
-//   name: "default name",
-//   price: 9999,
-// };
+Product.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+Product.defaultProps = {
+  img: defaultImage,
+  name: "default-name",
+  price: 14.98,
+};
 
 export default Product;
